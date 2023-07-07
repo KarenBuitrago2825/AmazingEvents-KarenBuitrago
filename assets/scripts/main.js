99852dfc27c5bcbe0b18258a88c8f0125c06f5bc
KarenBuitrago2825 <karenbuitrago2519@gmail.com>
2023-07-06 20:23:29 -0500

    Sprint-2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,7 @@ let contenedorTarjetas = document.getElementById("sectionTarjetas");
 function crearTarjeta(objeto) {
   return `
   <div class="col-12 col-md-6 col-xl-4 ">
-  <div class="card h-100">
+  <div class="card">
       <img src=${objeto.image} class="card-img-top w-100 h-50" alt="food fair">
       <div class="card-body">
           <h5 class="card-title">${objeto.name}</h5>
